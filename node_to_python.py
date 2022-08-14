bf66e58e9821cf25f77d3b5d99a2a14439eb2dd3
BrendanParmer <51296046+BrendanParmer@users.noreply.github.com>
2022-08-14 16:10:27 -0500

    feat: script sets most node attributes automatically now

--- node_to_python.py
+++ node_to_python.py
@@ -31,6 +31,104 @@ dont_set_defaults = {'NodeSocketCollection',
                      'NodeSocketTexture',
                      'NodeSocketVirtual'}
 
+node_settings = {
+    #attribute
+    "GeometryNodeAttributeStatistic" : ["data_type", "domain"],
+    "GeometryNodeCaptureAttribute" : ["data_type", "domain"],
+    "GeometryNodeAttributeDomainSize" : ["component"],
+    "GeometryNodeStoreNamedAttribute" : ["data_type", "domain"],
+    "GeometryNodeAttributeTransfer" : ["data_type", "mapping"],
+
+    #color
+    "ShaderNodeMixRGB" : ["blend_type", "use_clamp"],
+
+    #curve
+    "GeometryNodeCurveToPoints" : ["mode"],
+    "GeometryNodeFillCurve" : ["mode"], 
+    "GeometryNodeFilletCurve" : ["mode"],
+    "GeometryNodeResampleCurve" : ["mode"],
+    "GeometryNodeSampleCurve" : ["mode"],
+    "GeometryNodeTrimCurve" : ["mode"],
+    "GeometryNodeCurveHandleTypeSelection" : ["mode", "handle_type"],
+    "GeometryNodeSetCurveHandlePositions" : ["mode"],
+    "GeometryNodeCurveSetHandles" : ["mode", "handle_type"],
+    "GeometryNodeCurveSplineType" : ["spline_type"],
+
+    #curve primitives
+    "GeometryNodeCurveArc" : ["mode"],
+    "GeometryNodeCurvePrimitiveBezierSegment" : ["mode"],
+    "GeometryNodeCurvePrimitiveCircle" : ["mode"],
+    "GeometryNodeCurvePrimitiveLine" : ["mode"],
+    "GeometryNodeCurvePrimitiveQuadrilateral" : ["mode"],
+
+    #geometry
+    "GeometryNodeDeleteGeometry" : ["domain", "mode"],
+    "GeometryNodeDuplicateElements" : ["domain"],
+    "GeometryNodeProximity" : ["target_element"],
+    "GeometryNodeMergeByDistance" : ["mode"],
+    "GeometryNodeRaycast" : ["data_type", "mapping"],
+    "GeometryNodeSeparateGeometry" : ["domain"],
+
+    #input
+    "GeometryNodeCollectionInfo" : ["transform_space"],
+    "GeometryNodeObjectInfo" : ["transform_space"],
+    "GeometryNodeInputNamedAttribute" : ["data_type"],
+
+    #mesh
+    "GeometryNodeExtrudeMesh" : ["mode"],
+    "GeometryNodeMeshBoolean" : ["operation"],
+    "GeometryNodeMeshToPoints" : ["mode"],
+    "GeometryNodeSubdivisionSurface" : ["uv_smooth", "boundary_smooth"],
+    "GeometryNodeTriangulate" : ["quad_method", "ngon_method"],
+    "GeometryNodeScaleElements" : ["domain", "scale_mode"],
+
+    #mesh primitives
+    "GeometryNodeMeshCone" : ["fill_type"],
+    "GeometryNodeMeshCylinder" : ["fill_type"],
+    "GeometryNodeMeshCircle" : ["fill_type"],
+    "GeometryNodeMeshLine" : ["mode"],
+
+    #point
+    "GeometryNodeDistributePointsOnFaces" : ["distribute_method"],
+    "GeometryNodePointsToVolume" : ["resolution_mode"],
+
+    #text
+    "GeometryNodeStringToCurves" : ["overflow", "align_x", "align_y", 
+                                    "pivot_mode"],
+    
+    #texture
+    "ShaderNodeTexBrick" : ["offset", "offset_frequency", "squash", 
+                            "squash_frequency"],
+    "ShaderNodeTexGradient" : ["gradient_type"],
+    "GeometryNodeImageTexture" : ["interpolation", "extension"],
+    "ShaderNodeTexMagic" : ["turbulence_depth"],
+    "ShaderNodeTexNoise" : ["noise_dimensions"],
+    "ShaderNodeTexVoronoi" : ["voronoi_dimensions", "feature", "distance"],
+    "ShaderNodeTexWave" : ["wave_type", "bands_direction", "wave_profile"],
+    "ShaderNodeTexWhiteNoise" : ["noise_dimensions"],
+
+    #utilities
+    "GeometryNodeAccumulateField" : ["data_type", "domain"],
+    "FunctionNodeAlignEulerToVector" : ["axis", "pivot_axis"],
+    "FunctionNodeBooleanMath" : ["operation"],
+    "ShaderNodeClamp" : ["clamp_type"],
+    "FunctionNodeCompare" : ["data_type", "operation", "mode"],
+    "GeometryNodeFieldAtIndex" : ["data_type", "domain"],
+    "FunctionNodeFloatToInt" : ["rounding_mode"],
+    "ShaderNodeMapRange" : ["data_type", "interpolation_type", "clamp"], 
+    "ShaderNodeMath" : ["operation", "use_clamp"],
+    "FunctionNodeRandomValue" : ["data_type"],
+    "FunctionNodeRotateEuler" : ["type", "space"],
+    "GeometryNodeSwitch" : ["input_type"],
+
+    #vector
+    "ShaderNodeVectorMath" : ["operation"],
+    "ShaderNodeVectorRotate" : ["rotation_type", "invert"],
+
+    #volume
+    "GeometryNodeVolumeToMesh" : ["resolution_mode"]
+}
+
 class NodeToPython(bpy.types.Operator):
     bl_idname = "object.node_to_python"
     bl_label = "Node to Python"
@@ -82,10 +180,10 @@ class NodeToPython(bpy.types.Operator):
             #initialize node group
             file.write(f"{outer}#initialize {ng_name} node group\n")
             file.write(f"{outer}def {ng_name}_node_group():\n")
-            file.write(f"{inner}{ng_name}" +
-                f"= bpy.data.node_groups.new(" +
-                f"type = \"GeometryNodeTree\"," +
-                f"name = \"{node_group.name}\")\n")
+            file.write((f"{inner}{ng_name}"
+                        f"= bpy.data.node_groups.new("
+                        f"type = \"GeometryNodeTree\", "
+                        f"name = \"{node_group.name}\")\n"))
             file.write("\n")
 
             #initialize nodes
@@ -98,8 +196,9 @@ class NodeToPython(bpy.types.Operator):
                     for input in node.outputs:
                         if input.bl_idname != "NodeSocketVirtual":
                             file.write(f"{inner}#input {input.name}\n")
-                            file.write(f"{inner}{ng_name}.inputs.new" +
-                                f"(\"{input.bl_idname}\", \"{input.name}\")\n")
+                            file.write((f"{inner}{ng_name}.inputs.new"
+                                        f"(\"{input.bl_idname}\", "
+                                        f"\"{input.name}\")\n"))
                             if input.bl_idname in default_sockets:
                                 socket = node_group.inputs[input.name]
                                 if input.bl_idname == 'NodeSocketColor':
@@ -113,76 +212,79 @@ class NodeToPython(bpy.types.Operator):
                                     dv = socket.default_value
                                 
                                 #default value
-                                file.write(f"{inner}{ng_name}" +
-                                             f".inputs[\"{input.name}\"]" + 
-                                             f".default_value = {dv}\n")
+                                file.write((f"{inner}{ng_name}"
+                                            f".inputs[\"{input.name}\"]"
+                                            f".default_value = {dv}\n"))
                                 if input.bl_idname in value_sockets:
                                     #min value
-                                    file.write(f"{inner}{ng_name}" +
-                                                 f".inputs[\"{input.name}\"]" +
-                                                 f".min_value = " +
-                                                 f"{socket.min_value}\n")
+                                    file.write((f"{inner}{ng_name}"
+                                                f".inputs[\"{input.name}\"]"
+                                                f".min_value = "
+                                                f"{socket.min_value}\n"))
                                     #max value
-                                    file.write(f"{inner}{ng_name}" +
-                                                 f".inputs[\"{input.name}\"]" +
-                                                 f".max_value = " +
-                                                 f"{socket.max_value}\n")
+                                    file.write((f"{inner}{ng_name}"
+                                                f".inputs[\"{input.name}\"]"
+                                                f".max_value = "
+                                                f"{socket.max_value}\n"))
                             file.write("\n")
                     file.write("\n")
                 elif node.bl_idname == 'NodeGroupOutput':
                     file.write(f"{inner}#{ng_name} outputs\n")
                     for output in node.inputs:
                         if output.bl_idname != 'NodeSocketVirtual':
-                            file.write(f"{inner}{ng_name}.outputs" +
-                                         f".new(\"{output.bl_idname}\", " +
-                                         f"\"{output.name}\")\n")
+                            file.write((f"{inner}{ng_name}.outputs"
+                                        f".new(\"{output.bl_idname}\", "
+                                        f"\"{output.name}\")\n"))
                     file.write("\n")
 
                 #create node
                 node_name = node.name.lower()
                 node_name = node_name.replace(' ', '_').replace('.', '_')
                 file.write(f"{inner}#node {node.name}\n")
-                file.write(f"{inner}{node_name} " +
-                             f"= {ng_name}.nodes.new(\"{node.bl_idname}\")\n")
-                file.write(f"{inner}{node_name}.location " + 
-                             f"= ({node.location.x}, {node.location.y})\n")
-                file.write(f"{inner}{node_name}.width, {node_name}.height " +
-                             f"= {node.width}, {node.height}\n")
+                file.write((f"{inner}{node_name} "
+                            f"= {ng_name}.nodes.new(\"{node.bl_idname}\")\n"))
+                file.write((f"{inner}{node_name}.location "
+                            f"= ({node.location.x}, {node.location.y})\n"))
+                file.write((f"{inner}{node_name}.width, {node_name}.height "
+                            f"= {node.width}, {node.height}\n"))
                 if node.label:
                     file.write(f"{inner}{node_name}.label = \"{node.label}\"\n")
 
                 #special nodes
-                """
-                NTS: these aren't special
-                pretty much every node has something similar
-                you're gonna need to make some sort of dictionary with all 
-                the different quirky features for each node.
-                """
-                if node.bl_idname == 'GeometryNodeGroup':
-                    file.write(f"{inner}{node_name}.node_tree = " +
-                                 f"bpy.data.node_groups" +
-                                 f"[\"{node.node_tree.name}\"]\n")
+                print(node.bl_idname)
+                if node.bl_idname in node_settings:
+                    for setting in node_settings[node.bl_idname]:
+                        print(type(getattr(node, setting)))
+                        attr = getattr(node, setting)
+                        if type(attr) == str:
+                            attr = f"\'{attr}\'"
+                        file.write((f"{inner}{node_name}.{setting} = "
+                                    f"{attr}\n"))
+                elif node.bl_idname == 'GeometryNodeGroup':
+                    file.write((f"{inner}{node_name}.node_tree = "
+                                f"bpy.data.node_groups"
+                                f"[\"{node.node_tree.name}\"]\n"))
                 elif node.bl_idname == 'ShaderNodeValToRGB':
                     color_ramp = node.color_ramp
                     file.write("\n")
-                    file.write(f"{inner}{node_name}.color_ramp.color_mode = " +
-                                 f"\'{color_ramp.color_mode}\'\n")
-                    file.write(f"{inner}{node_name}.color_ramp" +
-                                 f".hue_interpolation = " +
-                                 f"\'{color_ramp.hue_interpolation}\'\n")
-                    file.write(f"{inner}{node_name}.color_ramp.interpolation" +
-                                 f" = '{color_ramp.interpolation}'\n")
+                    file.write((f"{inner}{node_name}.color_ramp.color_mode = "
+                                f"\'{color_ramp.color_mode}\'\n"))
+                    file.write((f"{inner}{node_name}.color_ramp"
+                                f".hue_interpolation = "
+                                f"\'{color_ramp.hue_interpolation}\'\n"))
+                    file.write((f"{inner}{node_name}.color_ramp.interpolation "
+                                f"= '{color_ramp.interpolation}'\n"))
                     file.write("\n")
                     for i, element in enumerate(color_ramp.elements):
-                        file.write(f"{inner}{node_name}_cre_{i} = " +
-                                     f"{node_name}.color_ramp.elements" + 
-                                     f".new({element.position})\n")
-                        file.write(f"{inner}{node_name}_cre_{i}.alpha = " + 
-                                     f"{element.alpha}\n")
+                        file.write((f"{inner}{node_name}_cre_{i} = "
+                                    f"{node_name}.color_ramp.elements"
+                                    f".new({element.position})\n"))
+                        file.write((f"{inner}{node_name}_cre_{i}.alpha = "
+                                    f"{element.alpha}\n"))
                         col = element.color
                         r, g, b, a = col[0], col[1], col[2], col[3]
-                        file.write(f"{inner}{node_name}_cre_{i}.color = " +
-                                     f"({r}, {g}, {b}, {a})\n\n")
+                        file.write((f"{inner}{node_name}_cre_{i}.color = "
+                                    f"({r}, {g}, {b}, {a})\n\n"))
                 
                 for input in node.inputs:
                     if input.bl_idname not in dont_set_defaults:
@@ -197,13 +299,11 @@ class NodeToPython(bpy.types.Operator):
                         else:
                             dv = input.default_value
                         if dv is not None:
-                            file.write(f"{inner}{node_name}" +
-                                         f".inputs[\"{input.name}\"]" +
-                                         f".default_value = {dv}\n")
-                
+                            file.write((f"{inner}{node_name}"
+                                        f".inputs[\"{input.name}\"]"
+                                        f".default_value = {dv}\n"))
                 file.write("\n")
             
-
             #initialize links
             if node_group.links:
                 file.write(f"{inner}#initialize {ng_name} links\n")     
@@ -214,9 +314,9 @@ class NodeToPython(bpy.types.Operator):
                 output_node = link.to_node.name.lower().replace(' ', '_')
                 output_socket = link.to_socket.name
                 
-                file.write(f"{inner}{ng_name}.links.new({input_node}" +
-                             f".outputs[\"{input_socket}\"], " + 
-                             f"{output_node}.inputs[\"{output_socket}\"])\n")
+                file.write((f"{inner}{ng_name}.links.new({input_node}"
+                            f".outputs[\"{input_socket}\"], "
+                            f"{output_node}.inputs[\"{output_socket}\"])\n"))
             
             #create node group
             file.write("\n")
