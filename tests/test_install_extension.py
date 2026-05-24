8d55977804f981b6b910330c901725ea0ce37b9d
BrendanParmer <51296046+BrendanParmer@users.noreply.github.com>
2026-05-24 14:38:54 -0500

    test: cleanup functions moved to base

--- tests/test_install_extension.py
+++ tests/test_install_extension.py
@@ -1,24 +1,6 @@
 from ntp_test import NTPTest
 
 class TestExtension(NTPTest):
-    """
-    @classmethod
-    def setUpClass(cls):
-        repo = str(Path(__file__).parent.parent)
-
-        import bpy
-        bpy.ops.wm.read_factory_settings(use_factory_startup_app_template_only=True)
-
-        repo_module = "test_repo"
-        new_repo = bpy.context.preferences.extensions.repos.new(
-            name="NodeToPython",
-            module=repo_module,
-            custom_directory=repo,
-            source='USER'
-        )
-        cls.module_path = f"bl_ext.{repo_module}.NodeToPython"
-        bpy.ops.preferences.addon_enable(module=cls.module_path)
-    """
     def test_disable_enable(self):
         try:
             import bpy
@@ -39,9 +21,3 @@ class TestExtension(NTPTest):
         self.assertTrue(hasattr(bpy.context.scene, "ntp_material_slots"))
         self.assertTrue(hasattr(bpy.context.scene, "ntp_shader_node_group_slots"))
         self.assertTrue(hasattr(bpy.context.scene, "ntp_world_slots"))
-    """
-    @classmethod
-    def tearDownClass(cls):
-        import bpy
-        bpy.ops.preferences.addon_disable(module=cls.module_path)
-    """
